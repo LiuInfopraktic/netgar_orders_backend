1a9caf473fbc375b43c0f05f0e4a493b2060980c
Liu <lsantana@infopraktic.cat>
2024-01-19 11:57:27 +0100

    v.0.0.1

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'patata',
-  database: 'netgar_orders',
+  password: '',
+  database: 'netgar',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
