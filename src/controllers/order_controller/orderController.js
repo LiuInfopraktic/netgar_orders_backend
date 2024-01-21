305fa8fa1dc9fa7ed8d3768569022d7429345afb
Liu <lsantana@infopraktic.cat>
2024-01-21 11:52:35 +0100

    eliminacio debug + readmin production updates

--- src/controllers/order_controller/orderController.js
+++ src/controllers/order_controller/orderController.js
@@ -5,7 +5,6 @@ const orderService = require('../../services/order_service/orderService');
 ***************/
 const putOrder = async (req, res) => {
     const order = req.body;
-    console.log(order) // -> development
     if(order.name && order.tel && order.emp && order.type && order.dni){
         try{
             let response = await orderService.putOrder(order);
@@ -13,11 +12,6 @@ const putOrder = async (req, res) => {
             else res.status(401).send({error:"couldn't add the order"});
         } catch(e) {res.status(500).send({error:"Internal server error"});}
     } else res.status(422).send({error:"order details missing"})
-}
-
-const test = async (req, res) => {
-    console.log('a');
-    res.send({data:'OK'})
 }
 
 module.exports = { putOrder, test }
--- src/database/order_database/orderDatabase.js
+++ src/database/order_database/orderDatabase.js
@@ -1,12 +1,10 @@
 const conn = require('../mysql');
 
 const putOrder = async (order) => {
-    console.log(order)
     const connection = await conn.connection();
     let sql = `insert into orders (dni, name, company, phone_number, type, data) values (?,?,?,?,?,?)`;
     try{
         const [rows, fields] = await connection.execute(sql,[order.dni, order.name, order.emp, order.tel, order.type,new Date()]);
-        console.log(rows)
         return rows;
     }catch(err){
         console.log(err)
