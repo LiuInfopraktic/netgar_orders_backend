04102d2bc687766c0bfd46befa67c476f33d66c2
Liu <lsantana@infopraktic.cat>
2024-03-15 08:56:38 +0100

    db credentials

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '',
+  user: 'devel',
+  password: 'devel',
   database: 'netgar',
   waitForConnections: true,
   connectionLimit: 10,
